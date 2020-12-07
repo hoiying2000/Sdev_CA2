f5d6896f3db7f974058419170c023ff14ac0a442
Oluwatosin Sojinrin <x00168370@mytudublin.ie>
2020-12-07 03:11:31 +0000

    View Order

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ItemListView, ItemDetailView, SearchResultsListView, ItemCreateView, ItemUpdateView, ItemDeleteView
 
 urlpatterns = [
-    path('post/delete/', ItemDeleteView.as_view(), name = 'post_delete'),
+    path('delete/', ItemDeleteView.as_view(), name = 'post_delete'),
     path('post/edit/', ItemUpdateView.as_view(), name = 'post_edit'),
     path('post/new/', ItemCreateView.as_view(), name = 'post_new'),
     path('', ItemListView.as_view(), name = 'item_list'),
